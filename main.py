1c507ca94391d03bd4802f26b4b5fd560bc9ea12
alphacpc <alphacpc@gmail.com>
2022-06-26 17:20:30 +0000

    Updated design user and start design for chat

--- main.py
+++ main.py
@@ -12,23 +12,31 @@ def register():
 
 @app.route('/admin')
 def admin():
-    return render_template('pages/admin.html')
+    return render_template('pages/admin/admin.html')
 
 @app.route('/add')
 def add():
-    return render_template('pages/add_user.html')
+    return render_template('pages/admin/add_user.html')
 
 @app.route('/archive')
 def archive():
-    return render_template('pages/archive.html')
+    return render_template('pages/admin/archive.html')
 
 @app.route('/user')
 def user():
-    return render_template('pages/home.html')
+    return render_template('pages/user/home.html')
 
 @app.route('/tree')
 def tree():
-    return render_template('pages/tree.html')
+    return render_template('pages/user/tree.html')
+
+@app.route('/member')
+def member():
+    return render_template('pages/user/member.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('pages/user/chat.html')
 
 if __name__=='__main__':
     app.run(debug=True,port=5000)
